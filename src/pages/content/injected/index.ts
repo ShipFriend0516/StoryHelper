219184ba42cee715b6f30b01fe22eeed264c055a
JeongwooSeo <shipfriend4371@naver.com>
2025-05-07 12:48:06 +0900

    feat: credit에 개발 블로그 추가

--- src/pages/content/injected/index.ts
+++ src/pages/content/injected/index.ts
@@ -3,6 +3,9 @@ import('@pages/content/injected/textCounter');
 import altTager from '@pages/content/injected/altTager';
 import imageSize from '@pages/content/injected/imageSize';
 
-await imageSize();
-await altTager();
+(async () => {
+  await imageSize();
+  await altTager();
+})();
+
 console.log('StoryHelper Load Complete');
