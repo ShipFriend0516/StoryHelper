e69babe704177996d28a1ece1877213e1e41bd22
JeongwooSeo <shipfriend4371@naver.com>
2025-05-21 11:11:25 +0900

    feat: index에 Inject

--- src/pages/content/injected/index.ts
+++ src/pages/content/injected/index.ts
@@ -2,10 +2,12 @@ import('@pages/content/injected/keymap');
 import('@pages/content/injected/textCounter');
 import altTager from '@pages/content/injected/altTager';
 import imageSize from '@pages/content/injected/imageSize';
+import checkSEO from '@pages/content/injected/checkSEO';
 
 (async () => {
   await imageSize();
   await altTager();
+  await checkSEO();
 })();
 
 console.log('StoryHelper Load Complete');
