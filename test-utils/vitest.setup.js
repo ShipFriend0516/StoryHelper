a129db0486ce22221e228f198c83c6e1fe9e06a5
JeongwooSeo <shipfriend4371@naver.com>
2026-03-28 15:09:50 +0900

    fix: test 불일치 문제 해결

--- test-utils/vitest.setup.js
+++ test-utils/vitest.setup.js
@@ -1,2 +1,22 @@
 // Do what you need to set up your test
 console.log('setup test: vitest.setup.js');
+
+// Chrome extension API mock
+global.chrome = {
+  i18n: {
+    getMessage: key => key,
+  },
+  storage: {
+    local: {
+      get: () => Promise.resolve({}),
+      set: () => Promise.resolve(),
+    },
+    onChanged: {
+      addListener: () => {},
+      removeListener: () => {},
+    },
+  },
+  runtime: {
+    getURL: path => path,
+  },
+};
